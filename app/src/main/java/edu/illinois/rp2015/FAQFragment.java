598b40ff0768f0d47f0ce7a773244876680c4716
Krish Masand <krishmasand@gmail.com>
2015-09-21 16:46:17 -0500

    renamed FAQ fragment

--- app/src/main/java/edu/illinois/rp2015/FAQFragment.java
+++ app/src/main/java/edu/illinois/rp2015/FAQFragment.java
@@ -1,18 +1,41 @@
 package edu.illinois.rp2015;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-public class FAQFragment extends Fragment {
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+public class FaqFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.faq_fragment,container,false);
+        getActivity().getWindow().addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        final ListView faqList = (ListView) v.findViewById(R.id.lstFAQ);
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("FAQ");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> events, ParseException e) {
+                if (e == null) {
+                    FaqAdapter adapter = new FaqAdapter(getActivity(), R.layout.faq_item_list, events);
+                    faqList.setAdapter(adapter);
+                } else {
+                    Log.e("Error", e.getMessage());
+                }
+            }
+        });
         return v;
     }
 }
